52e8f7807bf491d58cc5b884a2470cae17174e52
Austin Dudzic <austin.dudzic@gmail.com>
2021-05-09 09:27:41 -0400

    finished code added deletion

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,25 +1,20 @@
-  const router = require('express').Router();
+const router = require('express').Router();
+//es6 module to get id
 const { v4: uuid } = require('uuid');
-const { createNewNote, validateNote, deleteNote } = require('../../lib/notes') 
+const { createNewNote, deleteNote } = require('../../lib/notes') 
 const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
-
 router.post('/notes', (req, res) => {
-    
     req.body.id = uuid();
-
-    if (!validateNote(req.body)) {
-        res.status(400).send('The note is not properly formatted.');
-    } else {
         const note = createNewNote(req.body, notes);
         res.json(note);
     }
-});
-
+);
+// BONUS 
 router.delete('/notes/:id', (req, res) => {
     const note = deleteNote(req.params.id, notes);
     res.json(note);
--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-
-// Initialize the app and create a port
 const app = express();
 
+const PORT = process.env.PORT || 3005;
 
-const PORT = process.env.PORT || 3001;
-
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-
 app.use(express.static('public'));
 
 
-
-
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`API is currently running on ${PORT}!`);
 });
